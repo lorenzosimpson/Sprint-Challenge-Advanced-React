49e05a0ad84f5bce8619a6f954c46389dc8cdece
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-04 19:44:32 -0400

    adds dynamic classes, background images

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Card = props => {
+    
+
+
     return (
-        <div className='card'>
+        <div className={`card ${props.country.toLowerCase()}`}>
             <h1>{props.name}</h1>
             <h4>{props.country}</h4>
         </div>
@@ -10,3 +13,8 @@ const Card = props => {
 }
 
 export default Card;
+
+// <div className={
+//             props.country === 'France' ? 'card france'
+//             : props.country === 'Brazil' ? 'card selected'
+//             : 'card'}></div>
