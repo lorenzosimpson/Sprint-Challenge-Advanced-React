7620225b6210fbb305f03d44fa99c46d6a02c888
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-04 11:44:51 -0400

    data renders from api in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    playersData: []
+  }
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      const playersData = res.data;
+      console.log(playersData)
+    })
+  }
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
-  );
+  )
+}
 }
 
 export default App;
