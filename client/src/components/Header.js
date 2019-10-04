670fd41c5bf7da2bf748d28f35142db899e9b3c6
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-04 13:48:42 -0400

    completes mvp

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
     }
     return (
         <div className='header'>
-            <h1 data-testid='header-text'>Women's World Cup Players</h1>
-            <div className='dark-mode-toggle'>
-            <div className={darkMode ? 'toggle toggled' : 'toggle'} onClick={toggle}></div>
+            <h1 data-testid='header-text'>Women's World Cup Players 🌍</h1>
+            <div className='dark-mode-toggle' onClick={toggle}>
+            <div className={darkMode ? 'toggle toggled' : 'toggle'}></div>
             </div>
             
         </div>
